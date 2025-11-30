efff963278afe48e335e6adbec0f70a5a9dbd4dd
Lakshya Kumar Pandey <113142629+Lakshya1997kp@users.noreply.github.com>
2025-11-30 13:51:27 +0000

    some basic notes about js done

--- Basics_of_javascript/test.js
+++ Basics_of_javascript/test.js
@@ -1,1 +1,43 @@
 console.log("Lakshya")
+
+//so that is how you kinda print in javascript ,
+/*
+now you might wonder : Q) is console a class or something and is log a function , yeah i know previous knowledge 
+kicks in lol !! , 
+so to answer it -> console is actually an object knows as built in "global object" ( i think we will see about it later so hold ur horses my dear)
+and YEAH log is a function or should i say method ( it is actually inside the console object)
+*/
+
+//as we start this language we might have few question , i mean given we know a 
+// lil programming
+
+//so fear not just ask and lets tackle those question TOGETHER !!!! HEHE
+/*
+Q): who is the inventor or javascript and when?
+ANS): so we just got cool answer to it , "BRENDAN EICH" created this language in "1995"
+      and the best thing is that he just created it in 10 days , to add more interactivity 
+      to webpages , while working at netscape. 
+      Firstly it was named "Mocha" and then "Livescript" and then finally "Javascript"
+
+
+Q): So is it an interpretted language or compiled language?
+ANS): so javascript or js is actually an interpretted language 
+
+Q):Is javascript a case sensetive language?
+ANS): Absolutely javascript is a case sensetive language,
+      that is it is affected by different way of writing like
+       upper case and lower case are different for it
+       eg: Name and name are two different things not same
+
+
+Q): Is javascript a dynamically typed language?
+ANS): Yep !! just like python it is also a dynamically typed language,
+      that is we do not have to explicitly write the type of any variable 
+      (we will discuss about everything dont worry)
+
+        I THINK THIS MUCH IS ENOUGH FOR TODAY WE WILL MEET IN OTHER BASICS !!
+
+                    BYE !!                             BYE!!
+
+
+*/ 
